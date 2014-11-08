e7731ba339f35692e6664dfae0d007244b0f94eb
JZP <JZP@JZP_PC>
2014-11-08 12:18:41 +0800

    图片轮播done

--- src/com/blackswan/fake/activity/HairDoActivity.java
+++ src/com/blackswan/fake/activity/HairDoActivity.java
@@ -1,29 +1,34 @@
 package com.blackswan.fake.activity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.os.Bundle;
+
 import com.blackswan.fake.R;
 import com.blackswan.fake.base.BaseActivity;
-import android.os.Bundle;
 
 public class HairDoActivity extends BaseActivity
 {
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_hairdo);
+		
+		this.initViews();
 	}
-
+	
 	@Override
-	protected void initViews() {
+	protected void initEvents() {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	protected void initEvents() {
-		// TODO Auto-generated method stub
+	protected void initViews() {
+		
 		
 	}
 	
