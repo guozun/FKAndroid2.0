7f82609eadc10fdc8f30787ef088cb0e2aef8995
ChenSongSong <512709720@qq.com>
2014-11-08 22:44:13 +0800

    barberlist

--- src/com/blackswan/fake/activity/barberactivity/BarberListActivity.java
+++ src/com/blackswan/fake/activity/barberactivity/BarberListActivity.java
@@ -3,7 +3,7 @@ package com.blackswan.fake.activity.barberactivity;
 import com.blackswan.fake.base.BaseActivity;
 
 public class BarberListActivity extends BaseActivity {
-
+	
 	@Override
 	protected void initViews() {
 		// TODO Auto-generated method stub
