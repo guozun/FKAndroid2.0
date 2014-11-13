a81b3ea8ca326ea49698d4b06197b1802907a86e
JzpWh <jzp.year@outlook.com>
2014-11-13 11:50:15 +0800

    我的主页初步完成上部
    Next：Tab标签

--- src/com/blackswan/fake/activity/HairDoDetailActivity.java
+++ src/com/blackswan/fake/activity/HairDoDetailActivity.java
@@ -46,6 +46,12 @@ public class HairDoDetailActivity extends BaseActivity implements
 	private DisplayImageOptions options;
 	private Button order;
 	private Button buttonBack;
+	//赞数  顶数
+	private TextView upNum;
+	private TextView downNum;
+	//赞 顶 按钮
+	private ImageView upImg;
+	private ImageView downImg;
 	// 进度条
 	private ProgressWheel mProgressWheel;
 	// ImageSwitch
@@ -78,11 +84,28 @@ public class HairDoDetailActivity extends BaseActivity implements
 		mProgressWheel = (ProgressWheel) findViewById(R.id.id_hairdo_detail_progress);
 		imageGone = (ImageView) findViewById(R.id.id_hairdo_detail_pic_cache);
 		buttonBack = (Button)findViewById(R.id.iv_hairdo_detail_back);
+		upNum = (TextView)findViewById(R.id.tv_hairdo_item_good);
+		downNum = (TextView)findViewById(R.id.tv_hairdo_item_bad);
+		upImg = (ImageView) findViewById(R.id.iv_hairdo_detail_good);
+		downImg = (ImageView) findViewById(R.id.iv_hairdo_detail_bad);
+		
 		
 		buttonBack.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				defaultFinish();
+			}
+		});
+		upImg.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				
+			}
+		});
+		downImg.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				
 			}
 		});
 		mProgressWheel.setVisibility(View.GONE);
--- src/com/blackswan/fake/activity/MyPageActivity.java
+++ src/com/blackswan/fake/activity/MyPageActivity.java
@@ -1,5 +1,15 @@
 package com.blackswan.fake.activity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import com.blackswan.fake.R;
 import com.blackswan.fake.activity.useractivity.LoginActivity;
 import com.blackswan.fake.activity.useractivity.MyCollectActivity;
@@ -7,171 +17,138 @@ import com.blackswan.fake.activity.useractivity.PersonalInfoActivity;
 import com.blackswan.fake.base.BaseActivity;
 import com.blackswan.fake.bean.UserInfo;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.Toast;
-
-public class MyPageActivity extends BaseActivity
-{
-	//用户信息
-	private UserInfo userInfo = null;
-	//我的头像
+public class MyPageActivity extends BaseActivity {
+	// 用户信息
+	private UserInfo userInfo = new UserInfo();
+	// 我的头像
 	private ImageView myHeadImageView;
-	//我的昵称栏
-	private LinearLayout myNmae;
-	//我的预约
-	private LinearLayout myAppraise;
-	//我的收藏
-	private LinearLayout myCollect;
-	//预约详情
-	private LinearLayout appraise;
-	//设置
-	private ImageView settingImageView;
+	// 我的昵称栏
+	private TextView myName;
+	private TextView myDisp;
+	private ImageView mySex;
+	// 我的收藏
+	private Button myCollect;
+	// 我的话题
+	private Button myTopic;
+	//
+	private TextView myHairShow;
+	// 设置
+	private ImageButton settingImageView;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
-		// TODO Auto-generated method stub
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_mypage);
+		if (userInfo != null) {
+			setContentView(R.layout.activity_mypage);
+		} else {
+			setContentView(R.layout.activity_mypage_nologin);
+		}
 		initControl();
-		setLisentener();
+		
+		
 	}
+
 	/**
 	 * 初始化控件
 	 */
-	public void initControl()
-	{
-		myHeadImageView = (ImageView) findViewById(R.id.myHead);
-		myNmae = (LinearLayout) findViewById(R.id.myName);
-		settingImageView = (ImageView) findViewById(R.id.iv_setting);
-		myAppraise = (LinearLayout) findViewById(R.id.myAppraise);
-		myCollect = (LinearLayout) findViewById(R.id.myCollect);
-		setAppraise((LinearLayout) findViewById(R.id.myAppraise));
-		
+	public void initControl() {
+		if (userInfo != null) {
+			myHeadImageView = (ImageView) findViewById(R.id.iv_mypage_head);
+			myName = (TextView) findViewById(R.id.tv_mypage_name);
+			myDisp = (TextView) findViewById(R.id.tv_mypage_disp);
+			mySex = (ImageView) findViewById(R.id.iv_mypage_sex);
+			myCollect = (Button) findViewById(R.id.bt_mypage_collect);
+			myTopic = (Button) findViewById(R.id.bt_mypage_topic);
+			myHairShow = (TextView) findViewById(R.id.tv_mypage_hairshow);
+			settingImageView = (ImageButton) findViewById(R.id.ib_mypage_detail);
+			//setLisentener();
+			initViews();
+		} else {
+			Button login = (Button) findViewById(R.id.mypage_nologin_login);
+			login.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					//去登陆
+					startActivity(LoginActivity.class);
+				}
+			});
+		}
 	}
+
 	/**
 	 * 为各个控件设置监听
 	 */
-	public void setLisentener()
-	{
-		myHeadImageView.setOnClickListener(new OnClickListener()
-		{
-			
+	private void setLisentener() {
+		myHeadImageView.setOnClickListener(new OnClickListener() {
+
 			@Override
-			public void onClick(View v)
-			{
-				//弹出修改头像的界面
+			public void onClick(View v) {
+				// 弹出修改头像的界面
 				
 			}
 		});
-		myNmae.setOnClickListener(new OnClickListener()
-		{
-			
+		myName.setOnClickListener(new OnClickListener() {
+
 			@Override
-			public void onClick(View v)
-			{
-				//判断是否登入，未登入跳转登入界面
-				if (userInfo==null)
-				{
-					Intent intent = new Intent(MyPageActivity.this, LoginActivity.class);
-					startActivity(intent);
-				}else {
-					//跳转到我的详细信息页
-					Intent intent = new Intent(MyPageActivity.this,PersonalInfoActivity.class);
-					startActivity(intent);
+			public void onClick(View v) {
+				// 判断是否登入，未登入跳转登入界面
+				if (userInfo == null) {
+					startActivity(LoginActivity.class);
+				} else {
+					startActivity(MyPageActivity.class);
 				}
 			}
 		});
-		settingImageView.setOnClickListener(new OnClickListener()
-		{
-			
+		settingImageView.setOnClickListener(new OnClickListener() {
+
 			@Override
-			public void onClick(View v)
-			{
-				//跳转至设置页面
-				Intent intent = new Intent(MyPageActivity.this, SelectCityActivity.class);
-				startActivity(intent);
-				
+			public void onClick(View v) {
+				startActivity(MyPageActivity.class);
 			}
 		});
-		myAppraise.setOnClickListener(new OnClickListener()
-		{
-			
+		myTopic.setOnClickListener(new OnClickListener() {
+
 			@Override
-			public void onClick(View v)
-			{
-				//判断是否登入，未登入跳转登入界面
-				if (userInfo==null)
-				{
-					Intent intent = new Intent(MyPageActivity.this, LoginActivity.class);
-					startActivity(intent);
-				}else {
-					//若已登入，我的预约详细信息刷新显示
+			public void onClick(View v) {
+				// 判断是否登入，未登入跳转登入界面
+				if (userInfo == null) {
+					startActivity(LoginActivity.class);
+				} else {
+					// 若已登入，我的预约详细信息刷新显示
 				}
-				
+
 			}
 		});
-		//收藏夹
-		myCollect.setOnClickListener(new OnClickListener()
-		{
-			
+		// 收藏夹
+		myCollect.setOnClickListener(new OnClickListener() {
+
 			@Override
-			public void onClick(View v)
-			{
-				//判断是否登入，为登入跳转到登入页面
-				if (userInfo==null)
-				{
-					Intent intent = new Intent(MyPageActivity.this, LoginActivity.class);
-					startActivity(intent);
-				}else {
-					//若已登入，跳转至收藏页面
-					Intent intent = new Intent(MyPageActivity.this,MyCollectActivity.class);
-					startActivity(intent);
-				}				
+			public void onClick(View v) {
+				// 判断是否登入，为登入跳转到登入页面
+				if (userInfo == null) {
+					startActivity(LoginActivity.class);
+				} else {
+					startActivity(MyCollectActivity.class);
+				}
 			}
 		});
 	}
-	private long exitTime = 0;
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		if (keyCode == KeyEvent.KEYCODE_BACK) {
-			exit();
-			return false;
-		}
-		return super.onKeyDown(keyCode, event);
-	}
-	
-	public void exit() {
-		if ((System.currentTimeMillis() - exitTime) > 2000) {
-			Toast.makeText(getApplicationContext(), "再按一次退出剃头",
-					Toast.LENGTH_SHORT).show();
-			exitTime = System.currentTimeMillis();
-		} else {
-			finish();
-			System.exit(0);
-		}
-	
-	}
+
 	@Override
 	protected void initViews() {
-		// TODO Auto-generated method stub
+		myHeadImageView.setImageResource(R.drawable.head);
+		myName.setText("名字");
+		myDisp.setText("简介");
 		
+		mySex.setImageResource(R.drawable.bg_famale);
+		myCollect.setText("我的收藏 100");
+		myTopic.setText("我的主题 100");
+		myHairShow.setText("我的发型秀 100");
 	}
+
 	@Override
 	protected void initEvents() {
-		// TODO Auto-generated method stub
-		
+
 	}
-	public LinearLayout getAppraise() {
-		return appraise;
-	}
-	public void setAppraise(LinearLayout appraise) {
-		this.appraise = appraise;
-	}
+
 }
